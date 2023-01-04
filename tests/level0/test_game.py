b0c61ec6d9f1241d5ebb7ca7d52d84de644ffac8
sumukshashidhar <sumukuuu@gmail.com>
2023-01-04 16:35:53 -0500

    tests[level0]: add more tests

--- tests/level0/test_game.py
+++ tests/level0/test_game.py
@@ -22,3 +22,43 @@ class TestResponseBit(unittest.TestCase):
         """
         rb = game.ResponseBit()
         self.assertTrue(rb.value == 0 or rb.value == 1)
+
+    def test_get_value(self):
+        """
+        Test the get_value method of the ResponseBit class
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        None
+
+        Raises
+        ------
+        AssertionError
+            If the value of the bit is not 0 or 1
+        """
+        rb = game.ResponseBit()
+        self.assertTrue(rb.get_value() == 0 or rb.get_value() == 1)
+    
+    def test_take_response(self):
+        """
+        Test the take_response method of the ResponseBit class
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        None
+
+        Raises
+        ------
+        AssertionError
+            If the response is not 0 or 1
+        """
+        rb = game.ResponseBit()
+        self.assertTrue(rb.take_response(0) == True or rb.take_response(1) == True)
