5e0f5651d7bdebcaec67b1cb76725c0e92010a2c
sumukshashidhar <sumukuuu@gmail.com>
2023-01-04 16:50:36 -0500

    feat: slightly update level0 game to allow for streaks

--- src/level0/game.py
+++ src/level0/game.py
@@ -20,6 +20,8 @@ class ResponseBit:
         """
         # set the value of the bit
         self.value = randint(0, 1)
+        # set the score of the game
+        self.score = 0
 
     def get_value(self) -> int:
         """
@@ -51,9 +53,25 @@ class ResponseBit:
             Whether you won or not
         """
         # check if the response is correct
-        return response != self.value
+        if response != self.value:
+            # if it is, we update the "score"
+            self.score += 1
+            # reset the value of the bit
+            self.value = randint(0, 1)
+            # and return True
+            return True
+        else:
+            # if it isn't, we return False
+            return False
 
 
 if __name__ == "__main__":
     rb = ResponseBit()
-    print(rb.get_value())
+    while True:
+        print(rb.get_value())
+        res = rb.take_response(int(input("Enter your response: ")))
+        if res:
+            print(f"You won! Your score is now {rb.score} :D. Let's continue.")
+        else:
+            print("You Lost! Game Over :(")
+
